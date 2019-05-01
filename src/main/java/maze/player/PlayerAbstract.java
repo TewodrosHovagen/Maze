6d1897e7f3f57d4eb67fed7f88a94fecadfa614a
Hovagen, Tedy (th0695) <th0695@intl.att.com>
2019-05-01 15:47:43 +0300

    [345245]add random player and new abstract class player

---
+++ src/main/java/maze/player/PlayerAbstract.java
@@ -0,0 +1,21 @@
+package maze.player;
+
+import java.util.Set;
+
+public abstract class PlayerAbstract implements PlayerInterface {
+
+    private int isHitWall = 0;
+    private Set<Integer> bookMarks;
+
+    @Override
+    public void hitWall() {
+        System.out.println("You hit the wall");
+        isHitWall+1;
+    }
+
+    @Override
+    public void hitBookmark(int seq) {
+        bookMarks.add(seq);
+    }
+}
+
---
+++ src/main/java/maze/player/PlayerRandom.java
@@ -0,0 +1,11 @@
+package maze.player;
+
+import maze.DirectionsEnum;
+
+public class PlayerRandom extends PlayerAbstract {
+
+    @Override
+    public DirectionsEnum move() {
+        return null;
+    }
+}
