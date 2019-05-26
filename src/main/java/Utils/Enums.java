4257b398a164bbaf2ac4cfb365468d74ecc5afa2
Hovagen,Tedy(th0695) <tedy.hovagen@intl.att.com>
2019-05-26 22:20:52 +0300

    [123123123] added directions enum class to utils

---
+++ src/main/java/Utils/Enums.java
@@ -0,0 +1,14 @@
+package Utils;
+
+public class Enums {
+
+    public enum DirectionsEnum {
+        NORTH, EAST, SOUTH, WEST, BOOKMARK
+
+
+    }
+
+    public enum WalkingDirectionsEnum {
+        STRAIGHT, RIGHT, BACK, LEFT, BOOKMARK
+    }
+}
