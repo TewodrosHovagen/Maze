7efdddb0bf24edbb231434bfa50c5b82c752315a
Brojak, Shoshi (sb5844) <sb5844@intl.att.com>
2019-05-27 23:25:21 +0300

    OutputLog.java - clean the comments

--- src/main/java/Utils/logging/OutputLog.java
+++ src/main/java/Utils/logging/OutputLog.java
@@ -9,31 +9,12 @@ import java.util.logging.Logger;
 
 public class OutputLog {
     private static String logPath;
-   // private static boolean printToScreen = false;
 
     public OutputLog(String path){
         logPath = path;
         File file = new File(logPath);
         writeToOutput("",false);
-
-//        if (file.delete()) {
-//            log.info("The previous file was deleted");
-//        } else {
-//            log.info("File was not deleted");
-//        }
     }
-//    public OutputLog(boolean printToScreen){
-//        this.printToScreen = true;
-//    }
-
-//    public OutputLog(String logPath, boolean printToScreen){
-//        this.logPath = logPath;
-//        writeToLogFile("",printToScreen);
-//    }
-//
-//    public static void deleteOutputLog() {
-//
-//    }
 
     public static void writeToOutput(String logRecord){
         writeToOutput(String.format("%s%n", logRecord),true);
